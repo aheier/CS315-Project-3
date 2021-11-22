6aedf2fc77ca0317a0a7c21ef5e8e2991dafd4ad
Anthony Heier <ajheie19@smumn.edu>
2021-11-21 20:45:34 -0600

    Add project files.

---
+++ Project 3/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿
+namespace Project_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.checkCaseSensitive = new System.Windows.Forms.CheckBox();
+            this.comboBoxLocation = new System.Windows.Forms.ComboBox();
+            this.textBoxFileName = new System.Windows.Forms.TextBox();
+            this.dataGrid = new System.Windows.Forms.DataGridView();
+            this.buttonSearch = new System.Windows.Forms.Button();
+            this.labelError = new System.Windows.Forms.Label();
+            this.Number = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.FileName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.FileSize = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.FilePath = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(42, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Select a Location";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(42, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "File Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(42, 89);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Case Sensitive";
+            // 
+            // checkCaseSensitive
+            // 
+            this.checkCaseSensitive.AutoSize = true;
+            this.checkCaseSensitive.Location = new System.Drawing.Point(163, 89);
+            this.checkCaseSensitive.Name = "checkCaseSensitive";
+            this.checkCaseSensitive.Size = new System.Drawing.Size(15, 14);
+            this.checkCaseSensitive.TabIndex = 3;
+            this.checkCaseSensitive.UseVisualStyleBackColor = true;
+            // 
+            // comboBoxLocation
+            // 
+            this.comboBoxLocation.FormattingEnabled = true;
+            this.comboBoxLocation.Location = new System.Drawing.Point(163, 25);
+            this.comboBoxLocation.Name = "comboBoxLocation";
+            this.comboBoxLocation.Size = new System.Drawing.Size(158, 21);
+            this.comboBoxLocation.TabIndex = 4;
+            this.comboBoxLocation.SelectedIndexChanged += new System.EventHandler(this.comboBoxLocation_SelectedIndexChanged);
+            // 
+            // textBoxFileName
+            // 
+            this.textBoxFileName.Location = new System.Drawing.Point(163, 60);
+            this.textBoxFileName.Name = "textBoxFileName";
+            this.textBoxFileName.Size = new System.Drawing.Size(158, 20);
+            this.textBoxFileName.TabIndex = 5;
+            this.textBoxFileName.TextChanged += new System.EventHandler(this.textBoxFileName_TextChanged);
+            // 
+            // dataGrid
+            // 
+            this.dataGrid.AllowUserToAddRows = false;
+            this.dataGrid.AllowUserToDeleteRows = false;
+            this.dataGrid.AllowUserToResizeColumns = false;
+            this.dataGrid.AllowUserToResizeRows = false;
+            this.dataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGrid.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Number,
+            this.FileName,
+            this.FileSize,
+            this.FilePath});
+            this.dataGrid.Location = new System.Drawing.Point(45, 141);
+            this.dataGrid.Name = "dataGrid";
+            this.dataGrid.ReadOnly = true;
+            this.dataGrid.RowHeadersVisible = false;
+            this.dataGrid.Size = new System.Drawing.Size(630, 150);
+            this.dataGrid.TabIndex = 6;
+            // 
+            // buttonSearch
+            // 
+            this.buttonSearch.Location = new System.Drawing.Point(45, 109);
+            this.buttonSearch.Name = "buttonSearch";
+            this.buttonSearch.Size = new System.Drawing.Size(75, 23);
+            this.buttonSearch.TabIndex = 7;
+            this.buttonSearch.Text = "Search";
+            this.buttonSearch.UseVisualStyleBackColor = true;
+            this.buttonSearch.Click += new System.EventHandler(this.buttonSearch_Click);
+            // 
+            // labelError
+            // 
+            this.labelError.AutoSize = true;
+            this.labelError.ForeColor = System.Drawing.Color.Red;
+            this.labelError.Location = new System.Drawing.Point(160, 114);
+            this.labelError.Name = "labelError";
+            this.labelError.Size = new System.Drawing.Size(52, 13);
+            this.labelError.TabIndex = 8;
+            this.labelError.Text = "Error Msg";
+            // 
+            // Number
+            // 
+            this.Number.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.ColumnHeader;
+            this.Number.HeaderText = "#";
+            this.Number.Name = "Number";
+            this.Number.ReadOnly = true;
+            this.Number.Width = 39;
+            // 
+            // FileName
+            // 
+            this.FileName.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.FileName.HeaderText = "File Name";
+            this.FileName.Name = "FileName";
+            this.FileName.ReadOnly = true;
+            this.FileName.Width = 79;
+            // 
+            // FileSize
+            // 
+            this.FileSize.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.FileSize.HeaderText = "File Size";
+            this.FileSize.Name = "FileSize";
+            this.FileSize.ReadOnly = true;
+            this.FileSize.Width = 71;
+            // 
+            // FilePath
+            // 
+            this.FilePath.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.FilePath.HeaderText = "File Path";
+            this.FilePath.Name = "FilePath";
+            this.FilePath.ReadOnly = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(711, 306);
+            this.Controls.Add(this.labelError);
+            this.Controls.Add(this.buttonSearch);
+            this.Controls.Add(this.dataGrid);
+            this.Controls.Add(this.textBoxFileName);
+            this.Controls.Add(this.comboBoxLocation);
+            this.Controls.Add(this.checkCaseSensitive);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Search a file";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox checkCaseSensitive;
+        private System.Windows.Forms.ComboBox comboBoxLocation;
+        private System.Windows.Forms.TextBox textBoxFileName;
+        private System.Windows.Forms.DataGridView dataGrid;
+        private System.Windows.Forms.Button buttonSearch;
+        private System.Windows.Forms.Label labelError;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Number;
+        private System.Windows.Forms.DataGridViewTextBoxColumn FileName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn FileSize;
+        private System.Windows.Forms.DataGridViewTextBoxColumn FilePath;
+    }
+}
+
---
+++ Project 3/Form1.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Project_3
+{
+    public partial class Form1 : Form
+    {
+        int id = 0;
+        string fileSearch = "";
+        bool caseSensitive = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBoxLocation.Items.Clear();
+            DriveInfo[] drives = DriveInfo.GetDrives();
+            foreach (DriveInfo drive in drives)
+            {
+                comboBoxLocation.Items.Add(drive.Name);
+            }
+            comboBoxLocation.Items.Add(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile));
+            comboBoxLocation.Items.Add(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments));
+            comboBoxLocation.Items.Add("All");
+            labelError.Text = "";
+            buttonSearch.Enabled = false;
+        }
+
+        private void buttonSearch_Click(object sender, EventArgs e)
+        {
+            id = 0;
+            dataGrid.Rows.Clear();
+            labelError.Text = "";
+            fileSearch = textBoxFileName.Text;
+            if (!(caseSensitive = checkCaseSensitive.Checked))
+            {
+                fileSearch = fileSearch.ToLower();
+            }
+            if (textBoxFileName.Text == "") return;
+            dataGrid.Rows.Clear();
+            try
+            {
+                if (comboBoxLocation.SelectedItem.ToString() == "All")
+                {
+                    foreach (DriveInfo drive in DriveInfo.GetDrives())
+                    {
+                        displayFiles(drive.Name);
+                    }
+                }
+                else
+                {
+                    displayFiles(comboBoxLocation.SelectedItem.ToString());
+                }
+                if (id == 0)
+                {
+                    labelError.Text = "No Matching File Name Found";
+                }
+            }
+            catch (ArgumentException ex)
+            {
+                labelError.Text = $"Invalid file path: {ex.Message}";
+            }
+        }
+
+        public void displayFiles(string path)
+        {
+            string[] paths;
+            try
+            {
+                paths = GetDirectoriesInFolder(path);
+            }
+            catch (UnauthorizedAccessException)
+            {
+                return;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Unhandled Exception:\n{ex}");
+                return;
+            }
+            if (paths.Length == 0)
+            {
+                foreach (string item in GetItemsInFolder(path))
+                {
+                    //MessageBox.Show(item);
+                    ShowFileInfo(item);
+                }
+                return;
+            }
+            foreach (string directory in paths)
+            {
+                displayFiles(directory);
+            }
+        }
+        private string[] GetItemsInFolder(string path)
+        {
+            return Directory.GetFiles(path);
+        }
+        private string[] GetDirectoriesInFolder(string path)
+        {
+            return Directory.GetDirectories(path);
+        }
+        private void ShowFileInfo(string path)
+        {
+            FileInfo file = new FileInfo(path);
+            if ((caseSensitive ? file.Name : file.Name.ToLower()) == fileSearch)
+            {
+                dataGrid.Rows.Add(++id, file.Name, formatSize(file.Length), file.FullName);
+            }
+        }
+        private string formatSize(long size)
+        {
+            long kb, mb, gb;
+            if ((kb = size / 1000) > 0)
+            {
+                if ((mb = size / 1000000) > 0)
+                {
+                    if ((gb = size / 1000000000) > 0)
+                    {
+                        return gb + " GB";
+                    }
+                    return mb + " MB";
+                }
+                return kb + " KB";
+            }
+            return size + " bytes";
+        }
+
+        private void comboBoxLocation_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBoxLocation.SelectedItem == null || textBoxFileName.Text == "")
+            {
+                buttonSearch.Enabled = false;
+                return;
+            }
+            buttonSearch.Enabled = true;
+        }
+
+        private void textBoxFileName_TextChanged(object sender, EventArgs e)
+        {
+            if (comboBoxLocation.SelectedItem == null || textBoxFileName.Text == "")
+            {
+                buttonSearch.Enabled = false;
+                return;
+            }
+            buttonSearch.Enabled = true;
+        }
+    }
+}
+
+/*
+ * 
+    public partial class Form1 : Form
+    {
+        private int TotalFiles = 0;
+        private double totalTime = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonBrowse_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            //dialog. = "c:\\";
+            //dialog.Filter = "All files (*.*)|*.*";
+
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = dialog.SelectedPath;
+                //MessageBox.Show(filePath);
+                textBoxFilePath.Text = filePath;
+            }
+        }
+
+        private void buttonSearch_Click(object sender, EventArgs e)
+        {
+            if (textBoxFilePath.Text == "") return;
+            TotalFiles = 0;
+            totalTime = 0;
+            double epoch = (double)(DateTime.UtcNow - new DateTime(1970, 1, 1)).TotalMilliseconds;
+
+            dataGrid.Rows.Clear();
+            try
+            {
+                displayFiles(textBoxFilePath.Text);
+
+                labelTotalFiles.Text = TotalFiles.ToString();
+
+                totalTime = ((DateTime.UtcNow - new DateTime(1970, 1, 1)).TotalMilliseconds - epoch);
+                //MessageBox.Show(totalTime.ToString());
+                //MessageBox.Show(formatTime(totalTime));
+                labelSearchTime.Text = formatTime(totalTime);
+            }
+            catch (ArgumentException ex)
+            {
+                MessageBox.Show($"Invalid file path: {ex}");
+            }
+        }
+
+        
+    }
+}
+*/
